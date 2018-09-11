028b6e17921ae2d2d95272b14b4814a92b0d175e
ue4prog <vladtet2000@mail.ru>
2018-09-11 23:23:34 +0300

    Nice task. rly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // PLEASE DON'T change function name
+const currencies  = ['H', 'Q', 'D', 'N', 'P'];
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0 || currency >= 10000) {
+        return currency <= 0 ? {} : {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    return [50, 25, 10, 5, 1].reduce((acc, coin, index) => {
+        while (currency - coin >= 0) {
+            acc[index]++;
+            currency -= coin;
+        }
+        return acc;
+    }, [0, 0, 0, 0, 0]).reduce((acc, coins, index) => {
+        if (coins !== 0) {
+            acc[currencies[index]] = coins
+        }
+        return acc
+    },{})
 }
